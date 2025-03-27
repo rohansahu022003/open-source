e74246945f3071f499c2857aa17094cd1fe1f193
Mohit Jindal <jindalm55@gmail.com>
2025-03-28 03:42:58 +0530

    Commit by Mohit

--- selectionsort.h
+++ selectionsort.h
@@ -1,2 +1,11 @@
 #pragma once
-void selectionsort();
+void selectionsort(int arr[], int n);
+void printArray(int arr[], int size);
+void main()
+{
+    int arr[] = {64, 25, 12, 22, 11};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    selectionsort(arr, n);
+    printf("Sorted array: \n");
+    printArray(arr, n);
+}
